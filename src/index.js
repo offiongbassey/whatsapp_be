bd9ee609b71b8ff3e844c0df707d0c20cc2422ab
Offiong Bassey <naijaspeed1@gmail.com>
2023-12-19 05:12:01 +0100

    custom logger

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import app from "./app.js";
+import logger from "./configs/logger.config.js";
 
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
-    console.log(`Server is listening to PORT ${ PORT }`);
+    logger.info(`Server is listening to PORT ${ PORT }`);
 });
