c99c405ca7c213fb0535db7734dce11c1988e734
Offiong Bassey <naijaspeed1@gmail.com>
2023-12-30 14:00:07 +0100

    video call functions completed

--- src/utils/SocketServer.js
+++ src/utils/SocketServer.js
@@ -10,6 +10,9 @@ socket.on('join', (user_id) => {
     }
     //send online users to frontend
     io.emit("get-online-users", onlineUsers);
+
+    //send socket id
+    io.emit('setup socket', socket.id);
     
 });
 
@@ -45,4 +48,25 @@ socket.on('stop typing', (conversation) => {
     socket.in(conversation).emit("stop typing");
 });
 
+//call
+socket.on("call user", (data) => {
+    let userId = data.userToCall;
+    let userSocketId = onlineUsers.find((user) => user.userId === userId);
+    io.to(userSocketId.socketId).emit('call user', {
+        signal: data.signal,
+        from: data.from,
+        name: data.name,
+        picture: data.picture,
+    });
+});
+
+//answer call 
+socket.on("answer call", (data) => {
+    io.to(data.to).emit('call accepted', data.signal);
+})
+
+//ending a call
+socket.on('end call', (id) => {
+    io.to(id).emit("end call");
+})
 }
